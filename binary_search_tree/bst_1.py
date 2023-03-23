729623648c799d1f4f2abc40a6782b5a49f02399
Jake Peterson <a3sir86@gmail.com>
2023-03-23 08:32:18 -0500

    Create bst_1.py

---
+++ binary_search_tree/bst_1.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.right = None
+        self.left = None
+
+
+def buildBST(nums):
+    return None
+
+
+def findLevel(root, findVal):
+    return None
