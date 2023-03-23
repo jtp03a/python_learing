e1df0175f491b711a608e172358a900ced935c76
Jake Peterson <a3sir86@gmail.com>
2023-03-23 09:39:01 -0500

    Update bst_1.py

--- binary_search_tree/bst_1.py
+++ binary_search_tree/bst_1.py
@@ -1,13 +1,34 @@
 class Node:
-    def __init__(self, val):
-        self.val = val
-        self.right = None
+    def __init__(self, key):
         self.left = None
-
+        self.right = None
+        self.val = key
+        
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.val == key:
+            return root
+        elif root.val < key:
+            root.right = insert(root.right, key)
+        else:
+            root.left = insert(root.left, key)
+    return root
 
 def buildBST(nums):
-    return None
+    
+    for i, num in enumerate(nums):
+        if i ==0:
+            r = Node(num)
+        else:
+            r = insert(r, num)
+    return r
 
 
 def findLevel(root, findVal):
     return None
+    
+numbers = [50, 30, 20 , 40, 70, 60, 80]
+
+buildBST(numbers)
