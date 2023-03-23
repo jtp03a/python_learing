ac4f90fcbeb12f0140cfeca146c95a9902fd613e
Jake Peterson <a3sir86@gmail.com>
2023-03-23 10:11:08 -0500

    Update bst_1.py

--- binary_search_tree/bst_1.py
+++ binary_search_tree/bst_1.py
@@ -15,6 +15,7 @@ def insert(root, key):
         else:
             root.left = insert(root.left, key)
     return root
+    
 
 def buildBST(nums):
     
@@ -24,11 +25,39 @@ def buildBST(nums):
         else:
             r = insert(r, num)
     return r
-
-
+    
 def findLevel(root, findVal):
-    return None
+    level = 0
+    level = search(root, findVal, level)
+    return level
+    
+def search(root, findVal, level):
+    if root is None or root.val == findVal:
+        return level
+    level += 1
+    if root.val < findVal:
+        return search(root.right, findVal, level)
+    else:
+        return search(root.left, findVal, level)
+   
+   
     
 numbers = [50, 30, 20 , 40, 70, 60, 80]
 
-buildBST(numbers)
+r = buildBST(numbers)
+
+result = findLevel(r, 80)
+
+print(result)
+
+numbers2 = [7, 3, 8, 9, 2, 6]
+
+r2 = buildBST(numbers2)
+
+result2 = findLevel(r, 6)
+
+print(result)
+
+result2 = findLevel(r, 16)
+
+print(result)
