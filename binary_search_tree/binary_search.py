b56a1bf0e725a60f4eff130a13a8eddfe2d7345d
Jake Peterson <a3sir86@gmail.com>
2023-03-29 13:27:31 -0500

    Create binary_search.py

---
+++ binary_search_tree/binary_search.py
@@ -0,0 +1,10 @@
+# Refer to README.md for the problem instructions
+
+
+def createOrderedList(uList):
+    oList = []
+    return oList                
+
+
+def binarySearch (lst, start, end, val): 
+    return -1
